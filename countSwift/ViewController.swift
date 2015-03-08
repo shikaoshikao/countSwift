66b839685af752c90ea849a366c8efbf9d61cd8c
shikaoshikao <wedliketothink@gmail.com>
2015-03-09 02:11:49 +0900

    メッセージ

--- countSwift/ViewController.swift
+++ countSwift/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var plus: UIButton!
+    @IBOutlet weak var minus: UIButton!
+    
+    var number : Int = 0
+    
+        override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func plus (sender: AnyObject){
+        number = number+1
+        label.text = NSString(format: "%d", number)
+        if number >= 10 {
+            label.textColor = UIColor.redColor()
+            plus.hidden = true
+        } else if number > -10{
+            label.textColor = UIColor.blackColor()
+            minus.hidden = false
+        }else if number <= -10 {
+            label.textColor = UIColor.blueColor()
+        }
+        
+    }
+    @IBAction func minus (sender: AnyObject){
+        number = number-1
+        label.text = NSString(format: "%d", number)
+        if number <= -10 {
+            label.textColor = UIColor.blueColor()
+            minus.hidden = true
+        }
+        else if number < 10 {
+            label.textColor = UIColor.blackColor()
+            plus.hidden = false
+        }
+        else if number >= 10 {
+            label.textColor = UIColor.redColor()
+        }
+    }
 
 }
 
